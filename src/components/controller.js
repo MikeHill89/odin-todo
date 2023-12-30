b86a73ba7376903328c492bc75bba5c8fe88216b
MikeHill89 <MikeHill89@no-reply.com>
2023-12-30 16:42:55 +0200

    Starting working on UI

--- src/components/controller.js
+++ src/components/controller.js
@@ -1,4 +1,14 @@
+import { Todo } from './todo.js';
 export const addButton = document.getElementById("addTodo");
 export const viewAllButton = document.getElementById("viewAll");
-export const searchTodoButton = document.getElementById("searchTodo");
 
+export function renderAllProjects(){
+    console.log("rendering all projects");
+    const main = document.getElementById("main"); 
+    main.textContent = "";
+    Todo.todos.forEach(todo => {
+        const p = document.createElement("p");
+        p.textContent = todo.title;
+        main.appendChild(p);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import { addButton, searchTodoButton, viewAllButton } from './components/controller.js';
+import { addButton, viewAllButton, renderAllProjects } from './components/controller.js';
 import { createNewTodo } from './components/createTodo.js';
 import { Todo } from './components/todo.js';
 
 console.log("Hello World!");
 addButton.addEventListener('click',createNewTodo);
-viewAllButton.addEventListener('click',Todo.viewAll);
-searchTodoButton.addEventListener('click',Todo.searchTodo);
+viewAllButton.addEventListener('click',renderAllProjects);
 
